1e3342094ea9445b5b806273c4f94f32d992d6f3
Bhushan Chirmade <bhushanchirmade@gmail.com>
2016-07-28 21:56:30 -0500

    Intial project

---
+++ src/test/java/com/bbc/InitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = InitializrApplication.class)
+public class InitializrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
